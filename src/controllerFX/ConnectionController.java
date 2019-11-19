5bfb7a924316383bbb8ef2734764cd33b93f8dd4
Ayoub Mourabit <Ayoub.Mourabit.00@gmail.com>
2019-11-19 16:53:32 +0100

    Basic Pull Development

--- src/controllerFX/ConnectionController.java
+++ src/controllerFX/ConnectionController.java
@@ -1,24 +1,42 @@
 package controllerFX;
 
 import java.net.URL;
+
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class ConnectionController implements Initializable{
 
-	public Button connectionButton;
+	@FXML
+	private Button connectButton;
+	@FXML
+	private TextField pseudoField;
 
-	private void connectAction(MouseEvent event){
+	private String pseudo;
 
-
-	}
 
 	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
+	public void initialize(URL arg0, ResourceBundle arg1) {}
 
+	public void connectAction() throws Exception{
+		this.pseudo = pseudoField.getText();
+		Stage secondStage = (Stage) connectButton.getScene().getWindow();
+		Parent root = FXMLLoader.load(getClass().getResource("../viewFX/Chat.fxml"));
+		Scene scene = new Scene(root);
+		secondStage.setScene(scene);
+		secondStage.show();
+		//System.out.println(pseudo);
 	}
+
+
+
 }
--- src/controllerFX/Main.java
+++ src/controllerFX/Main.java
@@ -1,7 +1,9 @@
 package controllerFX;
-	
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,16 +12,16 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource("../viewFX/Connection.fxml"));
+			Scene scene = new Scene(root,700,600);
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Chat");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
